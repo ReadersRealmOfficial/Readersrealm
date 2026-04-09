715a04468050d6c92d6cce7e47721ea267834443
ReadersRealmOfficial <readersrealm.info@gmail.com>
2026-04-08 22:20:46 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.jsx'
+import Landing from './pages/Landing.jsx'
+import BookApp from './pages/BookApp.jsx'
+import Campfire from './pages/Campfire.jsx'
+import Admin from './pages/Admin.jsx'
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/app" element={<BookApp />} />
+          <Route path="/campfire" element={<Campfire />} />
+          <Route path="/adminaccess" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode><App /></React.StrictMode>
+)
